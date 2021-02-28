8c6155818ce6e895985a654170e88d3812d0e6cd
Jared Doval <jared.doval@patelhs.org>
2021-02-28 03:22:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+	{System.out.println("Hello Welcome to Jared's Cinema What movie would you like to watch?");
+
+ Scanner scan = double
+ movieTimes (time);
+ movieTimes (time);
+ if (blue == 12:30, time == 6:30)
+
+ System.out.println("Yay! Movies Starting. Grab your snacks & have a seat please");
+   return true; 
+ else 
+ System.out.println("Oh no! The Movie isn't available. Do you want to try another time?");
+  }
+}
