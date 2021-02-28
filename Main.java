8dbd7b1ac0ad17de47cfbb13cc912ff7d0f5ba0d
Jared Doval <jared.doval@patelhs.org>
2021-02-28 04:27:34 +0000

    I added some comments explaining my code such as "// this statement allows me to know what movie they will be watching to then search for its reviews

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ class Main {
 
 
   Scanner scan = new Scanner(System.in);
-  System.out.println("Hello and welcome to the Jareds Cinema. What is your name?");
+  System.out.println("Hello and welcome to the Jareds Cinema. What is your name?"); // this is a print staement telling me what their name is
   String name = scan.next();
 
-  System.out.println("Welcome to the cinema " + name + " please answer the questions down below.");
+  System.out.println("Welcome to the cinema " + name + " please answer the questions down below."); // this repeats their name and lets them know what to do with the rest of the survey 
 
-  System.out.println("The movies playing are 1 or 2, which one are you watching?");
+  System.out.println("The movies playing are 1 or 2, which one are you watching?"); // this statement allows me to know what movie they will be watching to then search for its reviews 
   int answer = scan.nextInt();
  
- System.out.println("We will now check the reviews on the movie you chose.");
+ System.out.println("We will now check the reviews on the movie you chose."); 
 
- System.out.println("The review of the movie you chose is " + movieReview(answer) + "% positive.");
+ System.out.println("The review of the movie you chose is " + movieReview(answer) + "% positive."); // this shows the percentage of the movies positive rating 
   }
 
 static int movieReview(int review){
